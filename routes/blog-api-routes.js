28124f45b08c281a536b4bf14da1e38bfba4e6a8
Julia Shreckhise <julia.shreckhise@gmail.com>
2018-05-21 22:07:39 -0400

    setup from testing-project2

---
+++ routes/blog-api-routes.js
@@ -0,0 +1,63 @@
+var db = require("../models");
+
+module.exports = function (app) {
+
+    //creating blog post
+    app.post("/api/newPost/:userID", (req, res) => {
+        console.log(req.body);
+
+        db.BlogPost.create(
+            {
+                title: req.body.title,
+                body: req.body.body,
+                UserId: req.params.userID
+            }
+        ).then((newPost) => {
+            res.json(newPost);
+        })
+            .catch((err) => {
+                console.log(err.errors[0].message);
+                res.status(500).send(err.errors[0].message);
+            });
+    });
+
+    //updating blog post ===========================NOT YET TESTED
+    app.put("/api/update/:postID", (req, res) => {
+        // NEED TITLE AND BODY
+        db.BlogPost.update({
+            title: title,
+            body: body,
+            where: {
+                id: req.params.postID,
+            }
+        }).then((updatedPost) => {
+            res.json(updatedPost);
+        });
+    });
+
+    //deleting blog post
+    app.delete("/api/delete/:postID", (req, res) => {
+
+        db.BlogPost.destroy({
+            where: {
+                id: req.params.postID
+            }
+        }).then((numDeleted) => {
+            res.status(200).send("success");
+        });
+    });
+
+    //choosing template ===================================NOT YET TESTED
+    app.put("/api/template/:templateID", (req, res) => {
+        // NEED USER ID (id)
+        db.User.update({
+            template: req.params.templateID,
+            where: {
+                id: id,
+            }
+        }).then((user) => {
+            res.json(user);
+        });
+    });
+
+}
---
+++ routes/html-routes.js
@@ -0,0 +1,144 @@
+var db = require("../models");
+
+module.exports = function (app) {
+    //login page
+    app.get("/", (req, res) => {
+        res.render("login");
+    });
+
+
+
+    //to get a user's blog 
+    app.get("/blogs/:username", (req, res) => {
+        var username = req.params.username;
+        var template;
+        var hbsObj;
+        var userID;
+
+        //get user
+        db.User.findOne({
+            where: {
+                username: username
+            }
+            //get user's chosen template
+        }).then((user) => {
+            console.log(user.dataValues);
+            template = "template" + user.dataValues.template;
+            console.log("template" + template);
+            userID = user.dataValues.id;
+            console.log("userID: " + userID);
+            //get user's blog posts
+        }).then(() => {
+            db.BlogPost.findAll({
+                where: {
+                    UserID: userID
+                }
+            }).then((blogPost) => {
+                var hbsObj = {
+                    username,
+                    blogPost
+                };
+                res.render(template, hbsObj);
+            });
+        });
+    });
+
+
+    //templates (examples)
+    app.get("/template/:id", (req, res) => {
+
+        template = "template" + req.params.id;
+
+        //just using for example
+        var hbsObj = {
+            username: "username",
+            blogPost: [{
+                title: "Post 1",
+
+                body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, ipsa voluptas delectus sed assumenda voluptates, ab adipisci perspiciatis earum magnam fugit quasi culpa repellendus totam in, unde neque sapiente quod praesentium nisi rerum enim laudantium aliquam. Dolore nobis quis, quas quam reiciendis. Nisi, minus quo accusamus. Perferendis tempore ducimus blanditiis quo error, nemo temporibus aperiam harum vero asperiores vitae magnam et dolores repudiandae exercitationem voluptatibus veniam rerum voluptas architecto alias culpa dolorem incidunt quasi fuga. Fugit amet totam modi, optio a quae architecto odio, officia ipsam, autem dolore dignissimos."
+            }, {
+                title: "Post2",
+                body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, ipsa voluptas delectus sed assumenda voluptates, ab adipisci perspiciatis earum magnam fugit quasi culpa repellendus totam in, unde neque sapiente quod praesentium nisi rerum enim laudantium aliquam. Dolore nobis quis, quas quam reiciendis. Nisi, minus quo accusamus. Perferendis tempore ducimus blanditiis quo error, nemo temporibus aperiam harum vero asperiores vitae magnam et dolores repudiandae exercitationem voluptatibus veniam rerum voluptas architecto alias culpa dolorem incidunt quasi fuga. Fugit amet totam modi, optio a quae architecto odio, officia ipsam, autem dolore dignissimos."
+
+            },
+            {
+                title: "Post3",
+                body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, ipsa voluptas delectus sed assumenda voluptates, ab adipisci perspiciatis earum magnam fugit quasi culpa repellendus totam in, unde neque sapiente quod praesentium nisi rerum enim laudantium aliquam. Dolore nobis quis, quas quam reiciendis. Nisi, minus quo accusamus. Perferendis tempore ducimus blanditiis quo error, nemo temporibus aperiam harum vero asperiores vitae magnam et dolores repudiandae exercitationem voluptatibus veniam rerum voluptas architecto alias culpa dolorem incidunt quasi fuga. Fugit amet totam modi, optio a quae architecto odio, officia ipsam, autem dolore dignissimos."
+
+            }]
+        }
+        res.render(template, hbsObj);
+    });
+
+
+    //blogSpace
+    app.get("/blogspace/:username", (req, res) => {
+        //need check to make sure user is logged in in order to view this
+
+        var username = req.params.username;
+
+        //find userid where user name then
+        var username = req.params.username;
+        var hbsObj;
+        var userID;
+
+        //get user
+        db.User.findOne({
+            where: {
+                username: username
+            }
+            //get user's id
+        }).then((user) => {
+            userID = user.dataValues.id;
+            console.log("userID: " + userID);
+            //get user's blog posts
+        }).then(() => {
+            db.BlogPost.findAll({
+                where: {
+                    UserID: userID
+                }
+            }).then((blogPost) => {
+                var hbsObj = {
+                    blogPost,
+                    userID,
+                    username
+                };
+                res.render("blogSpace", hbsObj);
+            });
+        });
+    });
+
+
+    //pg to newPost
+    app.get("/blogspace/:username/newPost", (req, res) => {
+        //need check to make sure user is logged in in order to view this
+
+        var username = req.params.username;
+
+        //find userid where user name then
+        var username = req.params.username;
+        var hbsObj;
+        var userID;
+
+        //get user
+        db.User.findOne({
+            where: {
+                username: username
+            }
+            //get user's id
+        }).then((user) => {
+            userID = user.dataValues.id;
+            console.log("userID: " + userID);
+    
+        }).then(() => {
+            var hbsObj = {
+                userID,
+                username
+            };
+            res.render("newPost", hbsObj);
+        });
+    });
+}
+
+
+
